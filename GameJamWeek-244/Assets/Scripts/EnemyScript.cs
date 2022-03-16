a608beb55d3ea7e50db58292130646b8c0276be6
Andrea Passini <andreapa98@gmail.com>
2022-03-16 20:29:34 +0100

    Enemy AI DecisionMaker

--- GameJamWeek-244/Assets/Scripts/EnemyScript.cs
+++ GameJamWeek-244/Assets/Scripts/EnemyScript.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+    public float LowHealth = 25f;
+    
+    [SerializeField] private float visionRange = 10f;
+    [SerializeField] LayerMask whatIsPlayer;
+
     private FSM _fsm;
 
+    private EnemyHealthController _enemyHealthController;
 
     // Start is called before the first frame update
     void Start()
     {
+        _enemyHealthController = GetComponent<EnemyHealthController>();
+        
         // State 1 Quisecent
         FSMState _quiescent = new FSMState();
 
+        // State 2 Attack
         FSMState _attack = new FSMState();
 
+        // State 3 Run Back to position
         FSMState _runAway = new FSMState();
 
         FSMTransition t1 = new FSMTransition(PlayerInRange);
+        FSMTransition t2 = new FSMTransition(PlayerNotInRange);
+        FSMTransition t3 = new FSMTransition(EnemyHealthLow);
 
         _fsm = new FSM(_quiescent);
     }
@@ -31,11 +43,51 @@ public class EnemyScript : MonoBehaviour
     #region FSM Transition
     public bool PlayerInRange()
     {
-        // Check if Player is in range
+        bool _inRange = false;
+        Vector3 _playerPos = new Vector3(0f, 0f, 0f);
 
+        // Check if Player is in range
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, visionRange, whatIsPlayer);
+        foreach (var hitCollider in hitColliders)
+        {
+            if (hitCollider.TryGetComponent(out HealthController h))
+            {
+                _playerPos = hitCollider.transform.position;
+                _inRange = true;
+            }
+                
+        }
+
+        if (!_inRange)
+        {
+            return false;
+        }
 
         // Check if Player is visible
+        RaycastHit hit;
+        Vector3 ray = _playerPos - transform.position;
+        if (Physics.Raycast(transform.position, ray, out hit))
+        {
+            if(hit.transform.tag == "Player")
+            {
+                return true;
+            }
+        }
 
+        return false;
+    }
+
+    public bool PlayerNotInRange()
+    {
+        return !PlayerInRange();
+    }
+
+    public bool EnemyHealthLow()
+    {
+        if(_enemyHealthController.GetHealth() < LowHealth)
+        {
+            return true;
+        }
 
         return false;
     }
