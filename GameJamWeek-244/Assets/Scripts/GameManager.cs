67e2f5496b4872ca57d6cada4ad0a87d3f6a8ab0
Andrea Passini <andreapa98@gmail.com>
2022-03-16 09:42:29 +0100

    Unity version regressed to ...20f1

--- GameJamWeek-244/Assets/Scripts/GameManager.cs
+++ GameJamWeek-244/Assets/Scripts/GameManager.cs
@@ -14,35 +14,6 @@ public class GameManager: MonoBehaviour
 		StartCoroutine(C_PlayStartScene());
 	}
 
-	public void PlayMainScene()
-	{
-		StartCoroutine(C_PlayMainScene());
-	}
-
-	public void PlayLvlStringente()
-    {
-		StartCoroutine(C_PlayLvlStringente());
-	}
-
-	public void PlayLvlFinale()
-    {
-		StartCoroutine(C_PlayLvlFinale());
-	}
-
-	public void PlayLvlMainMenu()
-    {
-		StartCoroutine(C_PlayLvlMainMenu());
-	}
-
-	public void PlayLvlDistorted()
-    {
-		StartCoroutine(C_PlayLvlMainMenu());
-	}
-
-	public void PlayLvlMain()
-	{
-		StartCoroutine(C_PlayLvlMain());
-	}
 
 	#region Coruotines
 
@@ -56,78 +27,6 @@ public class GameManager: MonoBehaviour
 
 		// Load new Scene
 		SceneManager.LoadScene("Start");
-	}
-
-	private IEnumerator C_PlayMainScene()
-	{
-		// Play animation
-		Animator.SetTrigger("transition");
-
-		// Wait for the end of the animation
-		yield return new WaitForSeconds(TransitionTime);
-
-		// Load new Scene
-		SceneManager.LoadScene("Main");
-	}
-
-	private IEnumerator C_PlayLvlStringente()
-	{
-		// Play animation
-		Animator.SetTrigger("transition");
-
-		// Wait for the end of the animation
-		yield return new WaitForSeconds(TransitionTime);
-
-		// Load new Scene
-		SceneManager.LoadScene("LivelloStringente");
-	}
-
-	private IEnumerator C_PlayLvlFinale()
-	{
-		// Play animation
-		Animator.SetTrigger("transition");
-
-		// Wait for the end of the animation
-		yield return new WaitForSeconds(TransitionTime);
-
-		// Load new Scene
-		SceneManager.LoadScene("Final");
-	}
-
-	private IEnumerator C_PlayLvlMainMenu()
-	{
-		// Play animation
-		Animator.SetTrigger("transition");
-
-		// Wait for the end of the animation
-		yield return new WaitForSeconds(TransitionTime);
-
-		// Load new Scene
-		SceneManager.LoadScene("MainMenu");
-	}
-
-	private IEnumerator C_PlayLvlDistorted()
-	{
-		// Play animation
-		Animator.SetTrigger("transition");
-
-		// Wait for the end of the animation
-		yield return new WaitForSeconds(TransitionTime);
-
-		// Load new Scene
-		SceneManager.LoadScene("Distorted");
-	}
-
-	private IEnumerator C_PlayLvlMain()
-	{
-		// Play animation
-		Animator.SetTrigger("transition");
-
-		// Wait for the end of the animation
-		yield return new WaitForSeconds(TransitionTime);
-
-		// Load new Scene
-		SceneManager.LoadScene("Main");
 	}
 
 	#endregion
