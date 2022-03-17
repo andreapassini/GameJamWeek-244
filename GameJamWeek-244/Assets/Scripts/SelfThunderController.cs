336c6869689136ce3011db3b674d27ec62c5a32a
Andrea Passini <andreapa98@gmail.com>
2022-03-17 12:12:43 +0100

    SelfThunderBolt

---
+++ GameJamWeek-244/Assets/Scripts/SelfThunderController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Shoot a thunder bolt from 5 points to the player
+public class SelfThunderController : MonoBehaviour
+{
+	public float ThunderBoltDuration = 1f;
+
+	// Thunder Bolt 
+	[SerializeField] GameObject thunderBoltPrefab;
+
+	[SerializeField] Transform spot1;
+	[SerializeField] Transform spot2;
+	[SerializeField] Transform spot3;
+	[SerializeField] Transform spot4;
+	[SerializeField] Transform spot5;
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(1)) {
+			SelfShootThunder();
+		}
+	}
+
+	public void SelfShootThunder()
+	{
+		ShootBolt(spot1, transform);
+		ShootBolt(spot2, transform);
+		ShootBolt(spot3, transform);
+		ShootBolt(spot4, transform);
+		ShootBolt(spot5, transform);
+
+	}
+
+	public void ShootBolt(Transform start, Transform end)
+	{
+		// Start LightningBolt
+		thunderBoltPrefab.transform.GetChild(0).position = start.position;
+
+		// End LightningBolt
+		thunderBoltPrefab.transform.GetChild(1).position = transform.position;
+
+		GameObject lightningBoltInst = Instantiate(thunderBoltPrefab);
+
+		StartCoroutine(LightningBoltDuration(lightningBoltInst));
+	}
+
+	IEnumerator LightningBoltDuration(GameObject lightningBoltToKill)
+	{
+		yield return new WaitForSeconds(ThunderBoltDuration);
+
+		Destroy(lightningBoltToKill);
+	}
+}
